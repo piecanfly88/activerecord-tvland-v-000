3689be262aa4b924784289ed7ee00976bda64be0
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-19 23:05:14 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,6 @@
 class CreateActors << ActiveRecord::Migration[5.1]
   def change
-    create_table :actors do |t|
+    create_table :actor do |t|
       t.string :firt_name
       t.string :last_name
     end
